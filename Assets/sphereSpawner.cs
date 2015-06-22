80e29773c2e4666656f7159bb162825e03cdf7ee
Alec Arme <Incubusalec@gmail.com>
2015-06-21 20:50:51 -0700

    Track files with GIT

---
+++ Assets/sphereSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphereSpawner : MonoBehaviour {
+	public GameObject spherePrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate (spherePrefab);
+	}
+}
---
+++ README.md.txt
@@ -0,0 +1,7 @@
+#BallPit
+
+Alec Arme and Toby Alden - June 2015
+
+Ballpit is a VR Demo built with Unity for Google Cardboard.
+Enjoy some smooth jazz while you're quickly overwhelmed with large plastic balls.
+
